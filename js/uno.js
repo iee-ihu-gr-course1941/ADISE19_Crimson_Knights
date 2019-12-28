901904c7fe874fabfb25aba0aae5549c96789ba1
Mr-Watch <57728190+Mr-Watch@users.noreply.github.com>
2019-12-28 20:07:08 +0200

    Patched some of the functions

--- js/uno.js
+++ js/uno.js
@@ -1,21 +1,20 @@
-var request_data;
-var hand;
+function draw_card(color, card_number, deck_number, element) {
+    var card, color_css, card_id;
+    switch (color) {
+        case '+':
+            card = '/images/+.png';
+            break;
+        case 'C':
+            card = '/images/C.png';
+            break;
+        case 'K':
+            card = '/images/K.png';
+            break;
+        default:
+            card = '/images/R'+card_number+'.png';
+            break;
 
-function draw_card(color, number) {
-    var card;
-    var color_css;
-    if (color === '+') {
-        card = '/MyProject/images/+.png';
-
-    } else if (color === 'C') {
-        card = '/MyProject/images/C.png';
-
-    } else if (color === 'K') {
-        card = '/MyProject/images/K.png';
-    } else {
-        card = '/MyProject/images/R' + number + '.png';
     }
-
     switch (color) {
         case 'R':
             color_css = 'red';
@@ -33,7 +32,9 @@ function draw_card(color, number) {
             color_css = 'special';
 
     }
-    $('#hand').append($('<img>', {
+    card_id = color + '/' + card_number + '/' + deck_number;
+    $(element).append($('<img>', {
+        id: card_id,
         class: color_css,
         src: card,
     }))
@@ -43,27 +44,38 @@ function ajax_request(request, method_parameter, function_parameter) {
     $.ajax({
         url: request,
         method: method_parameter,
+        headers: {
+            "X-Token": "36957abd1125613f4bbcbc859ab3c2a3"
+        },
         success: function (data) {
-            if (function_parameter == "draw_hand") {
+            if (function_parameter === "draw_hand") {
                 draw_hand(data);
             } else {
-                fetch(data);
+
             }
 
+            switch (function_parameter) {
+                case 'draw_hand':
+                    draw_hand(data, '#hand');
+                    break;
+                case 'draw_top_card':
+                    draw_hand(data, '#top_card');
+                    break;
+                case 'update_players':
+                    update_players(data);
+                    break;
+            }
+        },
+        error: function (data) {
+            alert(data.responseJSON.error_mysqli + "\n" + data.responseJSON.error_statement);
         }
     });
 
 }
 
-function draw_hand(data) {
-    hand = data;
-    
-    for(var i=0;i<data.length;i++){
-        var card = data[i];
-        draw_card(card.COLOR,card.NUMBER);
-    }
-}
 
-function fetch(data) {
-    request_data = data;
+function draw_hand(data, element) {
+    for (var i = 0; i < data.length; i++) {
+        draw_card(data[i].COLOR, data[i].NUMBER, data[i].DECK_NUM, element);
+    }
 }
